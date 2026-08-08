870ce6acc247a2bfd367cc9730a9b7a175752c2d
Admin Tata Usaha <tatausaha.mtsn1pku@gmail.com>
2026-08-08 07:15:22 +0700

    next update

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,20 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+      {
+        source: "/admin/",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
